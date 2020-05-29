e0ed40a6b4da83afd192daa76ad3d09eaa265549
wknorp <59899561+wknorp@users.noreply.github.com>
2020-05-29 11:54:01 -0700

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -2,7 +2,7 @@ $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
   
     $(".scrolltitle").css({
-      transform: "translate(0px, " + wScroll / 1.5 + "%)"
+      transform: "translate(0px, " + wScroll / 4.5 + "%)"
     });
   
     $(".transparent").css({
